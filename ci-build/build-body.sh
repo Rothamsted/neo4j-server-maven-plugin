0437fd9bc13fa3d4fc3e255e09bb824ca50b224d
Marco Brandizi <marco.brandizi@gmail.com>
2021-11-16 14:20:47 +0000

    Releasing 2.0 [ci skip]

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,6 +1,6 @@
 set -e 
 
-echo -e "\n\n\Running Maven Goal: $MAVEN_GOAL\n"
+echo -e "\n\n\tRunning Maven Goal: $MAVEN_GOAL\n"
 mvn --update-snapshots --batch-mode --show-version -Dgpg.skip -Dmaven.javadoc.skip=true \
     -Prres-deploy --settings ci-build/maven-settings.xml $MAVEN_ARGS $MAVEN_GOAL
 
