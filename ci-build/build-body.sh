cedad1f475f7fbb4a21136469f2c231f0fb94b00
Marco Brandizi <marco.brandizi@gmail.com>
2023-09-27 19:58:19 +0100

    CI, changing maven options variable.

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -2,20 +2,4 @@ set -e
 
 echo -e "\n\n\tRunning Maven Goal: $MAVEN_GOAL\n"
 mvn --update-snapshots --batch-mode --show-version -Dgpg.skip -Dmaven.javadoc.skip=true \
-    -Prres-deploy --settings ci-build/maven-settings.xml $MAVEN_ARGS $MAVEN_GOAL
-
-
-return
-
-# TODO: remove, this is now auto-executed as module.
-
-echo -e "\n\n\tIntegration Tests\n"
-cd integration-tests
-mvn verify
-cd ..
-
-if [[ "$MAVEN_GOAL" == "deploy" ]]; then
-	echo -e "\n\n\tDeployment\n"
-	mvn --update-snapshots --batch-mode -Prres-deploy \
-	    --settings ci-build/maven-settings.xml $MAVEN_ARGS deploy
-fi
+    -Prres-deploy --settings ci-build/maven-settings.xml $MAVEN_BUILD_ARGS $MAVEN_GOAL
